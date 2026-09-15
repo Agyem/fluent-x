0ca61ea6ad5ecff430bf4e504d0d0e53ff23495a
Nana Agyemang <gyemang@gmail.com>
2026-09-15 15:54:24 +0000

    Add dbcheck.py to gitignore

--- dbcheck.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-con = sqlite3.connect('database.db')
-print('tables:', [r[0] for r in con.execute("SELECT name FROM sqlite_master WHERE type='table'")])
-print('users:', con.execute('SELECT count(*) FROM users').fetchone()[0])
-print('cols:', [c[1] for c in con.execute('PRAGMA table_info(users)')])
-con.close()
